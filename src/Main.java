9d8bc69820e6dbbb6e89642b387d086405ce7dfa
Roman Krylov <0087897@gmail.com>
2022-08-09 18:33:54 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 public class Main {
     public static void main(String[] args) {
         int milkAmount = 200; // ml
@@ -15,12 +14,17 @@ public class Main {
         }
 
         // powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
-        System.out.println("Pancakes");
+        if (powderAmount >=400 && sugarAmount >= 10 && milkAmount >= 1000 && oilAmount >= 30) {
+            System.out.println("Pancakes");
+        }
 
         // milk - 300 ml, powder - 5 g, eggs - 5
-        System.out.println("Omelette");
+        if (milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5) {
+            System.out.println("Omelette");
+        }
 
         // apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
+        if (appleCount >= 3 && milkAmount >= 100 && powderAmount >= 300 && eggsCount >=4)
         System.out.println("Apple pie");
     }
 }
